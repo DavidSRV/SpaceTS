b37ca2c65e2b140e5a39e5fbd35cd99009f2e807
David Ruiz <david.ruizvargas@hotmail.com>
2022-12-24 08:13:55 -0500

    scafolding done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import Home from './pages/home/Home';
 import info from './utils/data.json'
 
-const spaceInfo = info;
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <Home/>
     </div>
   );
 }
