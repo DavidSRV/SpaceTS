31f6bd33b28c68ad47c3f08ad8d7afa7a3c32bae
David Ruiz <david.ruizvargas@hotmail.com>
2022-12-25 18:50:28 -0500

    Terminando home

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import './_navbar.scss';
-import icon from '../../assets/shared/logo.svg';
-import {Link} from 'react-router-dom'
+import React from "react";
+import "./_navbar.scss";
+import icon from "../../assets/shared/logo.svg";
+import { Link } from "react-router-dom";
 
-const NavBar:React.FC = () => {
+const NavBar: React.FC = () => {
   return (
-    <nav className='navBar'>
-      <div className='navBar__container'>
-        <img src={icon} alt="Icon" />
-        <hr />
-        <Link to='/'>Home</Link>
-        <Link to='/Destination'>Destination</Link>
-        <Link to='/Crew'>Crew</Link>
-        <Link to='/Technology'>Technology</Link>
+    <nav className="navBar">
+      <div className="navBar__container">
+        <img className="navBar__icon" src={icon} alt="Icon" />
+        <div className="navBar__links">
+          <Link to="/">Home</Link>
+          <Link to="/Destination">Destination</Link>
+          <Link to="/Crew">Crew</Link>
+          <Link to="/Technology">Technology</Link>
+        </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
