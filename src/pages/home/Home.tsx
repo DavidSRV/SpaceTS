847e2ea4c13eae7b029afb59aefcff56513db23a
David Ruiz <david.ruizvargas@hotmail.com>
2022-12-25 19:25:22 -0500

    TS

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import './_home.scss';
+import React from "react";
+import "./_home.scss";
 
-const Home:React.FC = () => {
+const Home: React.FC = () => {
   return (
-    <div>always</div>
-    )
-}
+    <main className="home">
+      <section className="home__description">
+        <p className="home__title">SPACE</p>
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </section>
+      <section className="home__button">
+          <p className="home__button__description">EXPLORE</p>
+      </section>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
