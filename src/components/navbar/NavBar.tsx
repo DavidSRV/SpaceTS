6bb8b8c7ff64e9570673346bf9f9e59f91bef902
David Ruiz <david.ruizvargas@hotmail.com>
2022-12-30 22:05:06 -0500

    Home basci done

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -9,10 +9,10 @@ const NavBar: React.FC = () => {
       <div className="navBar__container">
         <img className="navBar__icon" src={icon} alt="Icon" />
         <div className="navBar__links">
-          <Link to="/">Home</Link>
-          <Link to="/Destination">Destination</Link>
-          <Link to="/Crew">Crew</Link>
-          <Link to="/Technology">Technology</Link>
+          <Link to="/">00 Home</Link>
+          <Link to="/Destination">01 Destination</Link>
+          <Link to="/Crew">02 Crew</Link>
+          <Link to="/Technology">03 Technology</Link>
         </div>
       </div>
     </nav>
