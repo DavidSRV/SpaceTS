86782ce8f357b66b7ca296c31bba6f01c7a3d6b5
David Ruiz <david.ruizvargas@hotmail.com>
2023-01-03 23:27:25 -0500

    Typado de data

--- src/utils/Idata.ts
+++ src/utils/Idata.ts
@@ -1,4 +1,39 @@
+
 export interface Idata{
-    name:string,
-    images: string{}
+    destinations?:Idestinations,
+    crew?:Icrew,
+    technology?:Itechnology,
 }
+
+interface Iimages{
+    png:string,
+    webp:string
+}
+
+interface IimagesTechno{
+    portrait:string,
+    landscape:string
+}
+
+interface Idestinations{
+    name:string,
+    images: Iimages,
+    description:string,
+    distance:string,
+    travel:string
+}
+
+
+interface Icrew{
+    name:string,
+    images:Iimages,
+    role:string,
+    bio:string
+}
+
+interface Itechnology{
+    name:string,
+    images:IimagesTechno,
+    description:string
+}
+
