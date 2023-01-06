fcf4f32a123eaaaf6692fe64436e7852e5e74d79
David Ruiz <david.ruizvargas@hotmail.com>
2023-01-05 23:54:03 -0500

    TS

--- src/pages/destination/Destination.tsx
+++ src/pages/destination/Destination.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import data from '../../utils/data.json'
+import { Idata } from '../../utils/Idata'
 
 const Destination:React.FC = () => {
+
+
+
+
+
+
   return (
     <div>Destination</div>
   )
