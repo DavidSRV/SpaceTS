c63ac1761273d73b240bde39a0db6f2fd69043ab
David Ruiz <david.ruizvargas@hotmail.com>
2023-01-14 23:05:01 -0500

    TS

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,20 @@ import NavBar from "./components/navbar/NavBar";
 import Crew from "./pages/crew/Crew";
 import Destination from "./pages/destination/Destination";
 import Technology from "./pages/technology/Technology";
-import { Idata } from "./utils/Idata";
-import data from './utils/data.json'
+import data from "./utils/data.json"
 import { useState } from "react";
+import { Iinfo } from "./utils/Idata";
 
 function App(): JSX.Element {
 
-  const [crew, setCrew] = useState()
+  const [info,  setInfo] = useState<Iinfo["info"]>(data.info)
 
   return (
     <div className="App">
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Crew" element={<Crew />} />
+        <Route path="/Crew" element={<Crew info={info.crew}/>} />
         <Route path="/Destination" element={<Destination />} />
         <Route path="/Technology" element={<Technology />} />
       </Routes>
--- src/utils/Idata.ts
+++ src/utils/Idata.ts
@@ -1,37 +1,62 @@
-import { type } from "os"
+// interface Iimages {
+//   png: string;
+//   webp: string;
+// }
 
-interface Iimages{
-    png:string,
-    webp:string
+// interface IimagesTechno {
+//   portrait: string;
+//   landscape: string;
+// }
+
+// interface Idestinations {
+//   name: string;
+//   images: Iimages;
+//   description: string;
+//   distance: string;
+//   travel: string;
+// }
+
+// export interface Icrew {
+//   name: string;
+//   images: Iimages;
+//   role: string;
+//   bio: string;
+// }
+
+// interface Itechnology {
+//   name: string;
+//   images: IimagesTechno;
+//   description: string;
+// }
+
+export interface Iinfo {
+  info: {
+    destinations: {
+      name: string;
+      images: {
+        png: string;
+        webp: string;
+      };
+      description: string;
+      distance: string;
+      travel: string;
+    }[];
+    crew: {
+      name: string;
+      images: {
+        png: string;
+        webp: string;
+      };
+      role: string;
+      bio: string;
+    }[];
+    technology:{
+        name: string,
+        images: {
+          portrait: string,
+          landscape: string
+        },
+        description: string
+    }[]
+  };
 }
-
-interface IimagesTechno{
-    portrait:string,
-    landscape:string
-}
-
-interface Idestinations{
-    name:string,
-    images: Iimages,
-    description:string,
-    distance:string,
-    travel:string
-}
-
-interface Icrew{
-    name:string,
-    images:Iimages,
-    role:string,
-    bio:string
-}
-
-interface Itechnology{
-    name:string,
-    images:IimagesTechno,
-    description:string
-}
-
-export interface Idata {
-    data: Idestinations[] | Icrew[] | Itechnology[]
-}
-
