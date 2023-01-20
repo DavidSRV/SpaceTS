89b5c200cdfedea2491f0cb5629c9655c4df1d73
David Ruiz <david.ruizvargas@hotmail.com>
2023-01-20 00:52:04 -0500

    TS

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import NavBar from "./components/navbar/NavBar";
 import Crew from "./pages/crew/Crew";
 import Destination from "./pages/destination/Destination";
 import Technology from "./pages/technology/Technology";
-import data from "./utils/data.json";
 import { useState } from "react";
-// import { Iinfo } from "./utils/Idata";
 
 export interface IDestination {
   destination: {
@@ -19,12 +17,13 @@ export interface IDestination {
 }
 
 function App(): JSX.Element {
+
   const [destination, setDestination] = useState<IDestination["destination"]>([
     {
       name: "Moon",
       images: {
-        png: "./assets/destination/image-moon.png",
-        webp: "./assets/destination/image-moon.webp",
+        png: "../../assets/destination/image-moon.png",
+        webp: "../../assets/destination/image-moon.webp",
       },
       description:
         "See our planet as youve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
@@ -34,8 +33,8 @@ function App(): JSX.Element {
     {
       name: "Mars",
       images: {
-        png: "./assets/destination/image-mars.png",
-        webp: "./assets/destination/image-mars.webp",
+        png: "../../assets/destination/image-mars.png",
+        webp: "../../assets/destination/image-mars.webp",
       },
       description:
         "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
@@ -45,8 +44,8 @@ function App(): JSX.Element {
     {
       name: "Europa",
       images: {
-        png: "./assets/destination/image-europa.png",
-        webp: "./assets/destination/image-europa.webp",
+        png: "../../assets/destination/image-europa.png",
+        webp: "../../assets/destination/image-europa.webp",
       },
       description:
         "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
--- src/pages/destination/Destination.tsx
+++ src/pages/destination/Destination.tsx
@@ -5,11 +5,31 @@ const Destination = ({ destination }: IDestination): JSX.Element => {
   return (
     <>
       {destination.map((destination) => (
-        <div>
-          <h1>{destination.name}</h1>
-          <h2>{destination.images.png}</h2>
-          <p>{destination.distance}</p>
-        </div>
+        <section>
+          <article className="container__img">
+            <img src="" alt={destination.name} />
+          </article>
+
+          <article>
+            <div className="description__contaner">
+              <div className="description__scroll"></div>
+
+              <p className="description__name">{destination.name}</p>
+              <p className="description__description">
+                {destination.description}
+              </p>
+
+              <hr />
+
+              <div className="description__section">
+                <p className="description__kilometers">
+                  {destination.distance}
+                </p>
+                <p className="dsecription__days">{destination.travel}</p>
+              </div>
+            </div>
+          </article>
+        </section>
       ))}
     </>
   );
