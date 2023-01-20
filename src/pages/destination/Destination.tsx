061aebf53862e7cb70bc11a253a35ccb23d8d6ed
David Ruiz <david.ruizvargas@hotmail.com>
2023-01-19 23:58:23 -0500

    TS

--- src/pages/destination/Destination.tsx
+++ src/pages/destination/Destination.tsx
@@ -7,6 +7,8 @@ const Destination = ({ destination }: IDestination): JSX.Element => {
       {destination.map((destination) => (
         <div>
           <h1>{destination.name}</h1>
+          <h2>{destination.images.png}</h2>
+          <p>{destination.distance}</p>
         </div>
       ))}
     </>
