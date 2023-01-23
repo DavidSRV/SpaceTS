13f1818ff9fc2eb5e79ef6647a8e944ca84a8087
David Ruiz <david.ruizvargas@hotmail.com>
2023-01-22 23:41:32 -0500

    Section destination work

--- src/pages/destination/Destination.tsx
+++ src/pages/destination/Destination.tsx
@@ -1,42 +1,45 @@
 import React from "react";
 import { IDestination } from "../../utils/models/interfaces";
-import  "./_destinations.scss";
+import "./_destinations.scss";
 
 const Destination = ({ destination }: IDestination): JSX.Element => {
-
-
   return (
     <>
-      {destination.map((destination) => (
-        <section className="section">
+      {destination
+        .map((destination) => (
+          <section className="section">
 
-          <article className="section__containerImg">
-            <img src={destination.images.png} alt={destination.name} />
-          </article>
+            <article className="section__containerImg">
+              <img src={destination.images.png} alt={destination.name} />
+            </article>
 
-          <article>
-            <div className="section__description">
-              <div className="section__select">
+            <article>
+              <div className="section__containerInfo">
+                <div className="section__select">
+                  <hr className="section__button" />
+                  <hr className="section__button" />
+                  <hr className="section__button" />
+                  <hr className="section__button" />
+                </div>
 
-              </div>
-
-              <p className="section__name">{destination.name}</p>
-              <p className="section__description">
-                {destination.description}
-              </p>
-
-              <hr />
-
-              <div className="section__containerKmDays">
-                <p className="section__descriptionsKmDays">
-                  {destination.distance}
+                <p className="section__name">{destination.name}</p>
+                <p className="section__description">
+                  {destination.description}
                 </p>
-                <p className="section__days">{destination.travel}</p>
+
+                <hr />
+
+                <div className="section__containerKmDays">
+                  <p className="section__descriptionsKmDays">
+                    {destination.distance}
+                  </p>
+                  <p className="section__days">{destination.travel}</p>
+                </div>
               </div>
-            </div>
-          </article>
-        </section>
-      )).slice(0,1)}
+            </article>
+          </section>
+        ))
+        .slice(0, 1)}
     </>
   );
 };
