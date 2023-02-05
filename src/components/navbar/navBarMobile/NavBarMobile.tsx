594f1407bbcd23013a97a8ca4147e382b2082c84
David Ruiz <david.ruizvargas@hotmail.com>
2023-02-05 13:53:08 -0500

    NavBar Mobile

--- src/components/navbar/navBarMobile/NavBarMobile.tsx
+++ src/components/navbar/navBarMobile/NavBarMobile.tsx
@@ -1,16 +1,51 @@
-import React from 'react'
-import { IconButton } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu'
+import React, { useState } from "react";
+import { Drawer, IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Box } from "@mui/system";
+import CloseSharpIcon from "@mui/icons-material/CloseSharp"
 
 const NavBarMobile = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
   return (
     <>
-    <IconButton>
+      <IconButton
+        size="large"
+        edge="start"
+        color="primary"
+        aria-label="logo"
+        onClick={() => setIsDrawerOpen(true)}
+      >
+        <MenuIcon className="close" />
+      </IconButton>
 
-     <MenuIcon></MenuIcon>
-    </IconButton>
+      <Drawer
+        anchor="right"
+        open={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+      >
+        <Box
+          className="box"
+          p={2}
+          display="flex"
+          flexDirection="column"
+          justifyContent="right"
+          width="250px"
+          textAlign="center"
+          role="presentation"
+        >
+
+        <div className="links">
+            <IconButton className="button" onClick={() => setIsDrawerOpen(false)}>
+
+            <CloseSharpIcon />
+            </IconButton>
+        </div>
+
+        </Box>
+      </Drawer>
     </>
-  )
-}
+  );
+};
 
-export default NavBarMobile
+export default NavBarMobile;
