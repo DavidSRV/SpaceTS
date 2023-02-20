8215860fee4fd2d3811d3bd25a350acb3d1ef7d5
David Ruiz <david.ruizvargas@hotmail.com>
2023-02-19 23:19:37 -0500

    Add animation to img destionation

--- src/components/viewDestination/ViewDestination.tsx
+++ src/components/viewDestination/ViewDestination.tsx
@@ -3,14 +3,14 @@ import { PostitionContext } from "../../context/PostitionSlice";
 import { IpositionContextType } from "../../utils/models/interfaces";
 import "./_viewDestination.scss";
 import { Link } from "react-router-dom";
-import background from '../../assets/destination/background-destination-desktop.jpg'
+import background from "../../assets/destination/background-destination-desktop.jpg";
 
 const ViewDestination = (): JSX.Element => {
   let { numbers, info } = useContext(PostitionContext) as IpositionContextType;
 
-  useEffect(()=> {
-    document.body.style.backgroundImage = `url(${background})`
-  },[])
+  useEffect(() => {
+    document.body.style.backgroundImage = `url(${background})`;
+  }, []);
 
   return (
     <>
@@ -25,13 +25,22 @@ const ViewDestination = (): JSX.Element => {
             <article>
               <div className="section__containerInfo">
                 <div className="section__select">
-                  <Link to="/Destination/Moon" className="section__button" >Moon</Link>
+                  <Link to="/Destination/Moon" className="section__button">
+                    Moon
+                  </Link>
 
-                  <Link to="/Destination/Mars" className="section__button" > Mars</Link>
+                  <Link to="/Destination/Mars" className="section__button">
+                    {" "}
+                    Mars
+                  </Link>
 
-                  <Link to="/Destination/Europa" className="section__button">Europa</Link>
+                  <Link to="/Destination/Europa" className="section__button">
+                    Europa
+                  </Link>
 
-                  <Link to="/Destination/Titan" className="section__button" >Titan</Link>
+                  <Link to="/Destination/Titan" className="section__button">
+                    Titan
+                  </Link>
                 </div>
 
                 <p className="section__name">{destination.name}</p>
@@ -41,10 +50,10 @@ const ViewDestination = (): JSX.Element => {
 
                 <hr className="section__line" />
 
-                <div className="section__containerKmDays">
-                  <p className="section__Km">{destination.distance}</p>
-                  <p className="section__days">{destination.travel}</p>
-                </div>
+                    <div className="section__containerKmDays">
+                      <p className="section__Km">{destination.distance}</p>
+                      <p className="section__days">{destination.travel}</p>
+                    </div>
               </div>
             </article>
           </section>
